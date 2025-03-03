9e5bd9beefd1a89b2834a8edcaa2cdd8e3f82e35
Milanmiyani <milanmiyani2303@gmail.com>
2025-03-03 11:07:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'connection.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <!--header-->
+    <?php include 'header.php'?>
+
+    <div class="banner">
+      <img src="image/offerbanner2.png" alt="">
+    </div>
+   
+</body>
+</html>
